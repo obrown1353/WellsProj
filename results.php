6f8cee6083f0cc079694ff90ee4f9f5147cb4f88
mhinton2 <mhinton2@mail.umw.edu>
2026-03-22 20:24:59 -0400

    Replaced individual footer code with footer.php.

--- results.php
+++ results.php
@@ -660,35 +660,13 @@
         <div class="happy-toast">Volunteer registered successfully!</div>
     <?php endif ?>
 
-
     <!-- Footer -->
     <div style="width: 90%; height: 100%; outline: 1px #8DC9F7 solid; outline-offset: -0.5px; margin: 70px auto; padding: 1px 0;"></div>
 
-    <footer class="footer" style="margin-top: 100px;">
-        <div class="footer-left">
-            <img src="images/UMW_Eagles-logo.png" alt="Logo" class="footer-logo">
-            <div class="social-icons">
-                <a href="#"><i class="fab fa-facebook"></i></a>
-                <a href="#"><i class="fab fa-twitter"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
-                <a href="#"><i class="fab fa-linkedin"></i></a>
-            </div>
-        </div>
-        <div class="footer-right">
-            <div class="footer-section">
-		<div class="footer-topic">Connect</div>
-                <a href="https://www.facebook.com/profile.php?id=100086673730177#">Facebook</a>
-                <a href="https://www.instagram.com/umw_coe/">Instagram</a>
-                <a href="https://education.umw.edu/">Main Website</a>
-            </div>
-            <div class="footer-section">
-                <div class="footer-topic">Contact Us</div>
-                <a href="">mwells@umw.edu</a>
-                        <a href="tel:5406541290">(540) 654-1290</a>
-            </div>
-        </div>
-    </footer>
+    <?php require 'footer.php'; ?>
+
     <script src="https://kit.fontawesome.com/yourkit.js" crossorigin="anonymous"></script>
+
 </body>
 <?php endif ?>
 
@@ -934,31 +912,10 @@
     <!-- Footer -->
     <div style="width: 90%; height: 100%; outline: 1px #8DC9F7 solid; outline-offset: -0.5px; margin: 70px auto; padding: 1px 0;"></div>
 
-    <footer class="footer" style="margin-top: 100px;">
-        <div class="footer-left">
-            <img src="images/UMW_Eagles-logo.png" alt="Logo" class="footer-logo">
-            <div class="social-icons">
-                <a href="#"><i class="fab fa-facebook"></i></a>
-                <a href="#"><i class="fab fa-twitter"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
-                <a href="#"><i class="fab fa-linkedin"></i></a>
-            </div>
-        </div>
-        <div class="footer-right">
-            <div class="footer-section">
-                <div class="footer-topic">Connect</div>
-                <a href="https://www.facebook.com/profile.php?id=100086673730177#">Facebook</a>
-                <a href="https://www.instagram.com/umw_coe/">Instagram</a>
-                <a href="https://education.umw.edu/">Main Website</a>
-	    </div>
-            <div class="footer-section">
-                <div class="footer-topic">Contact Us</div>
-                <a href="">mwells@umw.edu</a>
-                <a href="tel:5406541290">(540) 654-1290</a>
-            </div>
-        </div>
-    </footer>
+    <?php require 'footer.php'; ?>
+
     <script src="https://kit.fontawesome.com/yourkit.js" crossorigin="anonymous"></script>
+
 </body>
 <?php endif ?>
 
@@ -1203,31 +1160,10 @@
     <!-- Footer -->
     <div style="width: 90%; height: 100%; outline: 1px #8DC9F7 solid; outline-offset: -0.5px; margin: 70px auto; padding: 1px 0;"></div>
 
-    <footer class="footer" style="margin-top: 100px;">
-        <div class="footer-left">
-            <img src="images/UMW_Eagles-logo.png" alt="Logo" class="footer-logo">
-            <div class="social-icons">
-                <a href="#"><i class="fab fa-facebook"></i></a>
-                <a href="#"><i class="fab fa-twitter"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
-                <a href="#"><i class="fab fa-linkedin"></i></a>
-            </div>
-        </div>
-        <div class="footer-right">
-            <div class="footer-section">
-                <div class="footer-topic">Connect</div>
-                <a href="https://www.facebook.com/profile.php?id=100086673730177#">Facebook</a>
-                <a href="https://www.instagram.com/umw_coe/">Instagram</a>
-                <a href="https://education.umw.edu/">Main Website</a>
-            </div>
-            <div class="footer-section">
-                <div class="footer-topic">Contact Us</div>
-                 <a href="mailto:mwells@umw.edu">mwells@umw.edu</a>
-                <a href="tel:5406541290">(540) 654-1290</a>
-            </div>
-        </div>
-    </footer>
+    <?php require 'footer.php';?>
+
     <script src="https://kit.fontawesome.com/yourkit.js" crossorigin="anonymous"></script>
+
 </body>
 <?php endif ?>
 
